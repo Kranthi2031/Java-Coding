3dcb25bde8b56739fd434c9720973b90bcf7b464
Kranthi2031 <kranthicheekati881@gmail.com>
2026-09-09 11:20:02 +0530

    Time: 11 ms (37.34%), Space: 46.9 MB (14.64%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        Map<Character, Character> map = new HashMap<>();
+        char currch = 'a';
+        for(char ch : key.toCharArray())
+        {
+            if(Character.isLetter(ch))
+                {
+                    if(!map.containsKey(ch))
+                    {
+                        map.put(ch, currch);
+                        currch++;
+                    }
+
+                }
+        }
+        map.put(' ', ' ');
+        String res ="";
+        for(char ch: message.toCharArray()){
+            res += map.get(ch);
+
+        }
+        return res;
+    }
+}
